2c1e9625ad0c4536d7a9fcab1ba580a70c5a2757
Eby Sofyan Fadly <eby.sofyan@gmail.com>
2019-10-29 23:07:54 +0800

    add several models for workout

---
+++ core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
+
+# Create your models here.
+
+
+class DescriptableModel(TimeStampedModel):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+
+    class Meta:
+        abstract = True
--- orm/models.py
+++ orm/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from . import member
+from . import club
+from . import equipment
+from . import region
+from . import presence
+from . import workout
 
 # Create your models here.
