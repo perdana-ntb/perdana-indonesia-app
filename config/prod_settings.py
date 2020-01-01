32104bd421c294ecda224478ffecc1f451c16606
ebysofyan <eby.sofyan@gmail.com>
2020-01-02 02:53:18 +0800

    comment staticfiles config

--- config/prod_settings.py
+++ config/prod_settings.py
@@ -12,11 +12,11 @@ if os.path.isfile(dotenv_file):
 DATABASE_URL_SCHEMA = os.environ.get('DATABASE_URL', 'postgres://perdana@localhost:5432/perdana_db')
 DATABASES['default'] = dj_database_url.parse(url=DATABASE_URL_SCHEMA, conn_max_age=600)
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    '/root/perdana-bucket/staticfiles'
-]
-STATIC_ROOT = os.path.join(STATICFILES_DIRS[0], 'root')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     '/root/perdana-bucket/staticfiles'
+# ]
+# STATIC_ROOT = os.path.join(STATICFILES_DIRS[0], 'root')
 
-MEDIA_URL = '/m/'
-MEDIA_ROOT = '/root/perdana-bucket/mediafiles'
+# MEDIA_URL = '/m/'
+# MEDIA_ROOT = '/root/perdana-bucket/mediafiles'
