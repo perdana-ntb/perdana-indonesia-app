f98b0150e490fd70c33a0c4e962c99b4e5f9d852
ebysofyan <eby.sofyan@gmail.com>
2020-12-26 11:52:40 +0800

    Chore : separate github workflow config

--- fabfile.py
+++ fabfile.py
@@ -12,6 +12,6 @@ CONNECTION_PROPERTIES = {
 @task
 def remote(ctx):
     with Connection(**CONNECTION_PROPERTIES) as c:
-        c.local("rm -rf /src/crom-platform/env")
+        c.local("rm -rf /src/perdana-indonesia-app/env")
         c.run("pwd")
         c.run("sh /src/scripts/deploy.sh")
