88d9a3aa6782a064b7861591e9fd9917f3e9d3a5
ebysofyan <eby.sofyan@gmail.com>
2021-01-22 21:42:01 +0800

    Add PracticeSchedule model, add club detail template

---
+++ perdana_indonesia/apps/club/choices.py
@@ -0,0 +1,9 @@
+WEEKDAY_CHOICES = (
+    (0, 'Senin'),
+    (1, 'Selasa'),
+    (2, 'Rabu'),
+    (3, 'Kamis'),
+    (4, 'Jumat'),
+    (5, 'Sabtu'),
+    (6, 'Ahad'),
+)
--- perdana_indonesia/apps/club/models.py
+++ perdana_indonesia/apps/club/models.py
@@ -3,6 +3,8 @@ from core.choices import CLUB_UNIT_TYPE_CHOICES
 from core.models import TimeStampedModel
 from django.db import models
 
+from club.choices import WEEKDAY_CHOICES
+
 
 class Club(TimeStampedModel):
     name = models.CharField(max_length=255)
@@ -51,3 +53,20 @@ class ArcheryRange (TimeStampedModel):
 
     def __str__(self):
         return self.name
+
+
+class PracticeSchedule(models.Model):
+    puslat = models.ForeignKey(Club, on_delete=models.CASCADE, null=True, blank=True)
+    archery_range = models.ForeignKey(
+        ArcheryRange, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    practice_day = models.CharField(max_length=5, choices=WEEKDAY_CHOICES, null=True, blank=True)
+    practice_start_time = models.TimeField(null=True, blank=True)
+    practice_end_time = models.TimeField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return 'Schedule of %s' % self.puslat.name
+
+    @property
+    def practice_day_name(self):
+        return WEEKDAY_CHOICES[self.practice_day][1]
